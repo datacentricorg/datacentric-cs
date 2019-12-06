53663b722a1eac4740caae8ef49d63919b3b15b7
Alexander Sokol <sokol@dualitygroup.com>
2019-12-06 14:41:04 -0500

    Corrected namespace following file move.

--- cs/src/DataCentric/Schema/Generator/Python/PythonKeyBuilder.cs
+++ cs/src/DataCentric/Schema/Generator/Python/PythonKeyBuilder.cs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-namespace DataCentric.Cli
+namespace DataCentric
 {
     /// <summary>
     /// Builder for generated python keys.
