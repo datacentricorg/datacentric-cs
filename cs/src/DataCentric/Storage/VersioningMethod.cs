981f2ebc29663baa261f2402ad2355439c788aae
Alexander Sokol <sokol@dualitygroup.com>
2020-01-20 02:23:07 -0500

    Cosmetic, improved comments.

--- cs/src/DataCentric/Storage/VersioningMethod.cs
+++ cs/src/DataCentric/Storage/VersioningMethod.cs
@@ -72,6 +72,10 @@ namespace DataCentric
         /// history of a record within the same dataset is not preserved and only
         /// a single record is permitted for a given dataset import list.
         ///
+        /// With this restriction, a single pass query without ordering by the record
+        /// or dataset TemporalId be used, resulting in better performance and simpler
+        /// query logic compared to NonTemporal versioning method.
+        ///
         /// For a given record and dataset key:
         ///
         /// * Within each dataset import list, there is only one version of the record.
